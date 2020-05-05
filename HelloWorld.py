b0ed83e6cb6da3b5c24ce2b25485761e011da932
Arnold Smithson <arnoldsmithson@outlook.com>
2020-05-04 23:31:27 -0600

    First Program

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+#Welcome to your first program!
+
+print("Hello World!")
+
+#There are three things introduced here:
+# <- This is a comment character! It makes the whole line invisible to Python.
+# print is a special keyword that pushes anything in its parenthesis to a console you can look at.
+# "Hello World!" Is known as a String. Strings are signified by the quotation marks.
